15cb1ce79fe18bb272354f52f1def6d2e1b68cb4
Bautista Tosi <tosibautista@gmail.com>
2022-12-13 14:46:55 -0300

    Correccion del string de la lista de productos disponibles

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ let productos = [
     { nombre: "DarkC", id: 6, categoria: "remera", stock: 20, precio: 20 },
     { nombre: "Celestyn", id: 7, categoria: "accesorio", stock: 3, precio: 20 },
     { nombre: "Celest", id: 8, categoria: "vestido", stock: 11, precio: 50 },
-    { nombre: "LightE", id: 8, categoria: "remera", stock: 16, precio: 15 },
-    { nombre: "LightB", id: 9, categoria: "vestido", stock: 6, precio: 50 },
-    { nombre: "LightE2", id: 10, categoria: "remera", stock: 7, precio: 20 }
+    { nombre: "LightE", id: 9, categoria: "remera", stock: 16, precio: 15 },
+    { nombre: "LightB", id: 11, categoria: "vestido", stock: 6, precio: 50 },
+    { nombre: "LightE2", id: 12, categoria: "remera", stock: 7, precio: 20 }
 ]
 //Funciones
 //distintas formas de pagar
