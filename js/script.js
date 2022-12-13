32f4c5a234ba5f973ce83e4f30f4ce0bf79b6f23
Bautista Tosi <tosibautista@gmail.com>
2022-12-12 21:06:34 -0300

    agragndo factura, comentando las funciones y arreglo de bugs

--- js/script.js
+++ js/script.js
@@ -1,54 +1,3 @@
-//funciones
-//distintas formas de pagar
-function pagar() {
-    let precio = productosElegidos[Elegido - 1].precio
-    do {
-        let pago = Number(prompt("Debe pagar: " + precio + "$"))
-        if (pago < precio) {
-            precio = precio - pago
-            faltapagar = true
-        } else if (pago > precio) {
-            let vuelto = precio - pago
-            alert("Su vuelto es de: " + (-vuelto) + "$")
-            alert("Disfrute su producto")
-            faltapagar = false
-            break
-        } else if (pago === precio) {
-            pago = precio - pago
-            alert("Disfrute su producto")
-            faltapagar = false
-            break
-        } else {
-            alert("ERROR")
-            faltapagar = true
-        }
-    } while (faltapagar === true)
-}
-function pagarSiONo() {
-    if (Elegido > 0 && Elegido <= productosElegidos.length) {
-        pagar()
-    } else if (Elegido === 0) {
-
-    } else {
-        alert("ERROR")
-    }
-}
-function listado(categoriaElegida) {
-    productosElegidos = productos.filter(productos => productos.categoria === categoriaElegida)
-    productosElegidos.sort(function (pro1, pro2) {
-        if (pro1.nombre < pro2.nombre) {
-            return -1;
-        }
-        if (pro1.nombre > pro2.nombre) {
-            return 1;
-        }
-        return 0;
-    })
-    listadoDeProductos = ""
-    for (let i = 0; i < productosElegidos.length; i++) {
-        listadoDeProductos += "\n" + (i + 1) + "." + productosElegidos[i].nombre
-    }
-}
 //array
 let productos = [
     { nombre: "JeansB", id: 1, categoria: "pantalon", stock: 4, precio: 20 },
@@ -63,63 +12,135 @@ let productos = [
     { nombre: "LightB", id: 9, categoria: "vestido", stock: 6, precio: 50 },
     { nombre: "LightE2", id: 10, categoria: "remera", stock: 7, precio: 20 }
 ]
+//funciones
+//distintas formas de pagar
+function pagar() {
+    /* let precio = productosElegidos[Elegido - 1].precio */
+    do {
+        let pago = Number(prompt("Debe pagar: " + precio + "$"))
+        if (pago < precio) {
+            precio = precio - pago
+            faltapagar = true
+        } else if (pago > precio) {
+            let vuelto = precio - pago
+            alert("Su vuelto es de: " + (-vuelto) + "$")
+            alert("Disfrute su compra")
+            faltapagar = false
+            break
+        } else if (pago === precio) {
+            pago = precio - pago
+            alert("Disfrute su compra")
+            faltapagar = false
+            break
+        } else {
+            alert("ERROR")
+            faltapagar = true
+        }
+    } while (faltapagar === true)
+}
+//agregar los productos comprados a la factura
+function agregarFactura() {
+    if (elegido > 0 && elegido <= productosElegidos.length) {
+        do {
+            cantidad = parseInt(prompt("¿Cuantos desea?\nDisponibles:" + productosElegidos[elegido - 1].stock))
+            if (cantidad > productosElegidos[elegido - 1].stock) {
+                alert("Lo sentimos no tenemos esa cantidad de productos\nIngrese una cantidad menor")
+            } else if (cantidad > 0 && cantidad <= productosElegidos[elegido - 1].stock) {
+                factura.push(productosElegidos[elegido - 1])
+                factura[pasadas].cant = cantidad
+                pasadas++
+                comprado = productos.find(producto => producto.id === productosElegidos[elegido - 1].id)
+                comprado.stock -= cantidad
+                break
+            } else {
+                alert("Ingrese el número correctamente")
+            }
+        } while (cantidad <= 0 || cantidad > productosElegidos[elegido - 1].stock)
+    } else if (elegido === 0) {
+    } else {
+        alert("ERROR")
+    }
+}
+//filtrar los productos de la categoria elegida
+function listado(categoriaElegida) {
+    productosElegidos = productos.filter(productos => productos.categoria === categoriaElegida)
+    productosElegidos.sort(function (pro1, pro2) {
+        if (pro1.nombre < pro2.nombre) {
+            return -1;
+        }
+        if (pro1.nombre > pro2.nombre) {
+            return 1;
+        }
+        return 0;
+    })
+    listadoDeProductos = ""
+    for (let i = 0; i < productosElegidos.length; i++) {
+        if (productosElegidos[i].stock > 0) {
+            listadoDeProductos += "\n" + (i + 1) + "." + productosElegidos[i].nombre + "    Precio: " + productosElegidos[i].precio + "$"
+        } else {
+
+        }
+    }
+}
 //codigo
 let faltapagar = true
 let productosElegidos = []
-let Elegido = 0
+let elegido = 0
 let listadoDeProductos = ""
 let compra = 1
 let pregunta = 0
-do {
+let factura = []
+let pasadas = 0
+let cantidad = 0
+while (pregunta !== 2 && compra !== 0) {
     pregunta = Number(prompt("BIENVENIDO USUARIO A UNA TIENDA DE ROPA \n¿Quiere comprar algo?\n1.Sí   2.No"))
     while (pregunta === 1 && compra !== 0) {
-        compra = Number(prompt("¿Qué desea de la tienda?\n1.Pantalones\n2.Remeras\n3.Accesorios\n4.Vestidos\n5.Medias\n0.Volver"))
+        compra = Number(prompt("¿Qué desea de la tienda?\n1.Pantalones\n2.Remeras\n3.Accesorios\n4.Vestidos\n5.Medias\n6.Pagar mi compra\n0.Dejar de comprar"))
         switch (compra) {
             case 1:
-                listado("pantalon")
                 do {
-                    Elegido = Number(prompt("Tenemos estos pantalones:" + listadoDeProductos + "\n0.volver"))
-                    pagarSiONo()
-                } while (Elegido !== 0 && faltapagar === true)
+                    listado("pantalon")
+                    elegido = Number(prompt("Tenemos estos pantalones:" + listadoDeProductos + "\n0.volver"))
+                    agregarSiONo()
+                } while (elegido !== 0 && faltapagar === true)
                 break
             case 2:
-                listado("remera")
                 do {
-                    Elegido = Number(prompt("Tenemos estas remeras:" + listadoDeProductos + "\n0.volver"))
-                    pagarSiONo()
-                } while (Elegido !== 0 && faltapagar === true)
+                    listado("remera")
+                    elegido = Number(prompt("Tenemos estas remeras:" + listadoDeProductos + "\n0.volver"))
+                    agregarSiONo()
+                } while (elegido !== 0 && faltapagar === true)
                 break
             case 3:
-                listado("accesorio")
                 do {
-                    Elegido = Number(prompt("Tenemos estos accesorios:" + listadoDeProductos + "\n0.volver"))
-                    pagarSiONo()
-                } while (Elegido !== 0 && faltapagar === true)
+                    listado("accesorio")
+                    elegido = Number(prompt("Tenemos estos accesorios:" + listadoDeProductos + "\n0.volver"))
+                    agregarSiONo()
+                } while (elegido !== 0 && faltapagar === true)
                 break
             case 4:
-                listado("vestido")
                 do {
-                    Elegido = Number(prompt("Tenemos estos vestidos:" + listadoDeProductos + "\n0.volver"))
-                    pagarSiONo()
-                } while (Elegido !== 0 && faltapagar === true)
+                    listado("vestido")
+                    elegido = Number(prompt("Tenemos estos vestidos:" + listadoDeProductos + "\n0.volver"))
+                    agregarSiONo()
+                } while (elegido !== 0 && faltapagar === true)
                 break
             case 5:
-                listado("medias")
                 do {
-                    Elegido = Number(prompt("Tenemos estas medias:" + listadoDeProductos + "\n0.volver"))
-                    pagarSiONo()
-                } while (Elegido !== 0 && faltapagar === true)
+                    listado("medias")
+                    elegido = Number(prompt("Tenemos estas medias:" + listadoDeProductos + "\n0.volver"))
+                    agregarSiONo()
+                } while (elegido !== 0 && faltapagar === true)
+            case 6:
+                break
             case 0:
                 break
             default:
                 alert("ERROR")
                 break
         }
-        if (compra==!0) {
-            pregunta=Number(prompt("¿Seguir comprando?\n1.Sí   2.No"))
-        }
     }
-} while (pregunta !== 2);
+}
 
 
 
